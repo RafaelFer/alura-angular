a1692799e15cf489e0294e7a7418cf5609eefcf9
Rafael Ferreira <rafael22fs@gmail.com>
2019-04-03 18:17:20 -0300

    Passando T e extends

--- alurabank/app/ts/views/MensagemView.ts
+++ alurabank/app/ts/views/MensagemView.ts
@@ -1,12 +1,4 @@
-class MensagemView{
-
-    private _elemento: Element;
-
-
-    constructor(seletor: string){
-        this._elemento = document.querySelector(seletor);
-    }
-
+class MensagemView extends View<string> {
 
     update(model: String): void {
         this._elemento.innerHTML = this.template(model);
